568383afbe9023d8c4222dedc5d5b7375a99b3d0
James Hayward <JamesJHayward99@gmail.com>
2025-02-05 22:13:56 +0000

    base of v2

---
+++ src/main_with_pandas.py
@@ -0,0 +1,19 @@
+# Copyright (c) 2025 James Hayward
+
+# IMPORTS
+import argparse
+import csv
+import os
+import re
+import sys
+
+# CLASS
+class TransportCalculator:
+    zone_prices
+    station_zones
+    user_data
+
+
+# MAIN PIPELINE
+if __name__ == "__main__":
+    pass
